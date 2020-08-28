8e0572ab4f4273a6fff0b2115e69f7c3ef67d8e8
Depak Borhara <depakborhara@gmail.com>
2020-08-28 02:01:43 -0400

    Added redux setup and superficial homepage component to render

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const middleware = [...getDefaultMiddleware(), logger];
+
+// TODO: Move out into seperate reducers folder.
+const reducer = {
+
+};
+
+const store = configureStore({
+  reducer,
+  middleware,
+  devTools: process.env.NODE_ENV !== 'production',
 });
+
+export default store;
